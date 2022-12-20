f6e301587615c28b181e6d926ea7bdfffd2ce2d6
Simplify360-DevOps <simplify360-devops@Simplify360-DevOpss-MacBook-Air.local>
2022-12-20 12:54:43 +0530

    aws cdk demo project first commit

--- lib/aws-cdk-project-stack.ts
+++ lib/aws-cdk-project-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class AwsCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, 'MyLambda', {
+      functionName: 'first-cdk-lambda',
+      code: new lambda.AssetCode('src'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 128
+    });
   }
 }
